b993bd51ecc16074f6c0b1b74ffa65fe9fb50464
d-jenkins <75498733+d-jenkins@users.noreply.github.com>
2021-06-08 22:17:05 -0500

    added postgress schema

---
+++ pgAdmin/schema.sql
@@ -0,0 +1,17 @@
+
+-- Create NBA Champs Table
+CREATE TABLE all_stats (
+  team TEXT,
+  season DATE,
+  Rel_Pace  DECIMAL(1),
+  Rel_ORtg DECIMAL(1),
+  Rel_DRtg DECIMAL(1),
+  Chip TEXT,
+  Predicted INT
+);
+
+
+
+-- Create NBA Champs Table
+
+DROP TABLE all_stats
