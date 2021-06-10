caaf12fca823d6a79decfdd1356a98d48faf2867
d-jenkins <75498733+d-jenkins@users.noreply.github.com>
2021-06-09 20:55:24 -0500

    App Folder Structure

---
+++ testapp.py
@@ -0,0 +1,59 @@
+# import dependencies
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+# Import pgadmin password from python file
+from champs import pw
+
+#################################################
+# Create Database Connection
+#################################################
+engine = create_engine(f'postgresql://postgres:{pw}@localhost:5432/nbachamps')
+#engine = create_engine(f'postgresql://{connection_string}')
+
+
+##########################################################
+# Setup Database
+##########################################################
+
+# engine = create_engine("nbaChamps")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Create references to tables
+nbaChamps = Base.classes.placeholder
+
+
+##########################################################
+# Setup Flask
+##########################################################
+
+# Create an app
+app = Flask(__name__)
+
+##########################################################
+# Decorate Flask Routes
+##########################################################
+
+# 3. Define the route
+@app.route("/")
+def home():
+    print("Server received request for 'home' page...")
+    return "Welcome to my 'Home' page! This is where I will introduce my website or project"
+
+@app.route("/about")
+def about():
+    print("Server received request for 'About' page...")
+    return "Welcome to my 'About' page!"
+
+if __name__ == "__main__":
+    app.run(debug=True)
