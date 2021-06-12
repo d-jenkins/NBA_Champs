7a86b10c31d566eeb474437460e0ccd1e86ef487
Eric Nietzer <75461451+Soup-or-Salad@users.noreply.github.com>
2021-06-12 13:21:33 -0500

    Update testapp.js

--- static/js/testapp.js
+++ static/js/testapp.js
@@ -1,16 +1,16 @@
-d3.csv("./.csv").then(function(nbaPlayers) {
+d3.csv("data/champs.csv").then(function(nbaTeams) {
 
-    console.log(nbaPlayers);
+    console.log(nbaTeams);
 
     // log a list of names
-    var names = nbaPlayers.map(data => data.name);
+    var names = nbaTeams.map(data => data.name);
     console.log("names", names);
 
-    // Cast each hours value in tvData as a number using the unary + operator
-    playerData.forEach(function(data) {
-        data.location = +data.location;
+    //
+    teamData.forEach(function(data) {
+        data.chip = +data.chip;
         console.log("Name:", data.name);
-        console.log("Location:", data.location);
+        console.log("Chip?:", data.chip);
     });
 }).catch(function(error) {
     console.log(error);
