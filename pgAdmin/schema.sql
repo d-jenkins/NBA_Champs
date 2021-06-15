609769e90b233470d028571416697c1474dbb316
d-jenkins <75498733+d-jenkins@users.noreply.github.com>
2021-06-14 19:28:07 -0500

    New Champs Schema

--- pgAdmin/schema.sql
+++ pgAdmin/schema.sql
@@ -1,5 +1,5 @@
 
--- Create NBA Champs Table
+-- Create all stats Table
 CREATE TABLE all_stats (
   id SERIAL PRIMARY KEY,	
   team TEXT,
@@ -8,11 +8,46 @@ CREATE TABLE all_stats (
   Rel_ORtg DECIMAL,
   Rel_DRtg DECIMAL,
   Chip TEXT
---   Predicted INT
+);
+
+
+--Drop  all stats table
+DROP TABLE all_stats
+
+
+
+-- Create Champs Table
+CREATE TABLE champs (
+  id SERIAL PRIMARY KEY,	
+  Team object,
+  Rel_Pace float64,
+  Rel_ORtg float64,
+  Rel_DRtg float64,
+  Playoffs float64,
+  Age object,
+  Ht. float64,
+  Wt. object,
+  FGA object,
+  FG% object,
+  3PA  object,
+  3P% object,
+  2PA object,
+  2P% object,
+  FTA object,
+  FT% object,
+  ORB object,
+  DRB object,
+  AST object,
+  STL object,
+  BL object,
+  TOV object,
+  PF object,
+  PTS object,
+  Predicted float64,
+  Error float64 
 );
 
 
 
--- Create NBA Champs Table
-
-DROP TABLE all_stats
+--Drop champs table
+DROP TABLE champs
